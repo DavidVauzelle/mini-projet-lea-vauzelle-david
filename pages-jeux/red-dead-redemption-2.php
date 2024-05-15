c0c062dd456e37635d5fbd9e274aad1f6e285253
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-05-15 09:21:59 +0200

    Update liens pages jeux

--- pages-jeux/red-dead-redemption-2.php
+++ pages-jeux/red-dead-redemption-2.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Voici mon deuxième projet"> 
     <title>Fiche de : Read Dead Redemption 2.</title>
-    <link rel="stylesheet" href="/style/reset.css">
-    <link rel="stylesheet" href="/style/pages_jeux.css">
-    <link rel="stylesheet" href="/style/nav.css">
-    <link rel="stylesheet" href="/style/footer.css">
+    <link rel="stylesheet" href="../style/reset.css">
+    <link rel="stylesheet" href="../style/pages_jeux.css">
+    <link rel="stylesheet" href="../style/nav.css">
+    <link rel="stylesheet" href="../style/footer.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Caesar+Dressing&family=Caveat:wght@400..700&family=Ceviche+One&display=swap" rel="stylesheet">
